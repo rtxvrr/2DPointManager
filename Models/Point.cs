f4e3d1c0b04ec02582a393f1aba1696c3d1c9c60
PC <PC@DESKTOP-JP6T437>
2024-02-22 19:15:17 +0300

    Add project files.

---
+++ Models/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DPointManager.Models
+{
+    public struct Point
+    {
+        public double X { get; set; } 
+        public double Y { get; set; } 
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"(X: {X}, Y: {Y})";
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using _2DPointManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _2DPointManager.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private double _xCoordinate;
+        private double _yCoordinate;
+
+        public MainViewModel()
+        {
+            Points = new ObservableCollection<Point>();
+            AddPointCommand = new RelayCommand(AddPoint, CanAddPoint);
+        }
+
+        public ObservableCollection<Point> Points { get; }
+
+        public double XCoordinate
+        {
+            get => _xCoordinate;
+            set
+            {
+                _xCoordinate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double YCoordinate
+        {
+            get => _yCoordinate;
+            set
+            {
+                _yCoordinate = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddPointCommand { get; }
+
+        private bool CanAddPoint() => _xCoordinate >= -10 && _xCoordinate <= 10 &&
+                                       _yCoordinate >= -10 && _yCoordinate <= 10;
+
+        private void AddPoint()
+        {
+            Points.Add(new Point(_xCoordinate, _yCoordinate));
+            XCoordinate = 0;
+            YCoordinate = 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
